807da3ab1a1f284820e0127afbfc15c11c3c379b
Antony <97451137+TonybynMp4@users.noreply.github.com>
2024-02-23 14:23:22 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 function addToCart(productId) {
   var quantity = document.getElementById("quantity_" + productId).value;
   var productName = document.querySelector("#" + productId + " h3").innerText;
-  var productPrice = document
-    .querySelector("#" + productId + " .bottomproduct p")
-    .innerText.split(" ")[1]; // Supposons que le prix soit formaté comme "Price: $X"
+  var productPrice = 2;
 
   var item = {
     name: productName,
     price: parseFloat(productPrice),
     quantity: parseInt(quantity),
   };
+
+console.log(productPrice)
 
   // Récupérer les articles existants dans le panier depuis le stockage local
   var existingCartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
